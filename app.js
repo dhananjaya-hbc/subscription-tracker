b77fce7c317cb7ee2038459aaf3d1aed607fa24a
Dhananjaya Chamod <171498451+Dhananjaya001@users.noreply.github.com>
2025-07-29 18:59:52 +0530

    create express server

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get('/', (req, res) => {
+    res.send('Hello subcription tracker!');
+});
+app.listen(3000, () => {
+    console.log('subcription tracker is running on http://localhost:3000');
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app;
